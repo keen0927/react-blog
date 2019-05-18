2ec2cd5d923b1c6c65d860aad9203ca0b4b6b4a2
KEEN <kyh0927@live.co.kr>
2019-05-19 02:34:43 +0900

    setting

---
+++ src/store/modules/post.js
@@ -0,0 +1,15 @@
+import { createAction, handleActions } from 'redux-actions';
+import { Map } from 'immutable';
+import { pender } from 'redux-pender';
+
+// 액션타입
+
+// 액션 생성
+
+// 초기값
+const initialState = Map({});
+
+// 리듀서
+export default handleActions({
+
+}, initialState);
